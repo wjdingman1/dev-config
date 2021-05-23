21e22c18a7bc51e55d26b20f41aaa871a951692c
Wes Dingman <wdingman@asymmetrik.com>
2021-05-23 14:22:19 -0400

    Add some comments to setup-env

--- dev-config/setup-env.bash
+++ dev-config/setup-env.bash
@@ -5,7 +5,7 @@ if ! brew list -1 | grep -q git; then
     brew install git
 fi
 
-#https://www.atlassian.com/git/tutorials/dotfiles
+# https://www.atlassian.com/git/tutorials/dotfiles
 config="usr/bin/git --git-dir=$HOME/.dev-config.git/ --worktree=$HOME"
 
 FILE=$HOME/.gitignore
@@ -13,16 +13,14 @@ if [ ! -f "$FILE" ]; then
     touch $FILE
 fi
 
+# Add bare repo to .gitignore
 if ! grep -q ".dev-config.git" $HOME/.gitignore; then
     echo ".dev-config.git" >> $HOME/.gitignore
 fi
 
+# Clone bare repo if it doesn't exist
 [[ -d $HOME/.dev-config.git ]] || git clone --bare git@github.com:wjdingman1/dev-config.git $HOME/.dev-config.git
 
+# Checkout bare repo, will overrwrite coexisting files in ~
 $config checkout
 $config config --local status.showUntrackedFiles no
-
-FILE=$HOME/.zshrc
-if [ -f "$FILE" ]; then
-    echo "alias config="$config"" >> $FILE
-fi
