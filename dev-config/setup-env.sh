608e618aa74990af19d375df01dc6736fd09e9d0
Wes Dingman <wes.dingman@h59qk3jjk6.myfiosgateway.com>
2022-07-05 09:52:34 -0400

    Update setup

--- dev-config/setup-env.sh
+++ dev-config/setup-env.sh
@@ -6,20 +6,10 @@ if ! brew list -1 | grep git; then
 fi
 
 # https://www.atlassian.com/git/tutorials/dotfiles
-config="usr/bin/git --git-dir=$HOME/.dev-config.git/ --worktree=$HOME"
-
-FILE=$HOME/.gitignore
-if [ ! -f "$FILE" ]; then
-    touch $FILE
-fi
-
-# Add bare repo to .gitignore
-if ! grep -q ".dev-config.git" $HOME/.gitignore; then
-    echo ".dev-config.git" >> $HOME/.gitignore
-fi
+config="git --git-dir=$HOME/.dev-config.git/ --work-tree=$HOME"
 
 # Clone bare repo if it doesn't exist
-[[ -d $HOME/.dev-config.git ]] || git clone --bare git@github.com:wjdingman1/dev-config.git $HOME/.dev-config.git
+git clone --bare git@github.com:wjdingman1/dev-config.git $HOME/.dev-config.git
 
 # Checkout bare repo, will overrwrite coexisting files in ~
 $config checkout
